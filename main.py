44097b6f780cef8fa342756ff417005e8b5d920e
Adrian <aab1g18@soton.ac.uk>
2022-05-13 14:10:55 +0100

    Added documentation to pre-training

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import torch.nn
-
-from models.addmul import HandleAddMul
 import torch as T
 import torch.nn as nn
 import numpy as np
+from models.addmul import HandleAddMul
+
 
 training_split = 0.8
 testing_split = 1 - training_split
@@ -19,7 +19,11 @@ output_dims = [20]
 batchsize = 128
 num_epochs = 20000
 
-# For running the add data
+'''Data Loading (Need to load )
+        Need to load datasets of same size otherwise batch size for multiplication dataset underfits (it's not a 
+        large dataset)
+'''
+
 data_fp = ["generate_datasets/tmp/digit-data/simple_mul.npy",
            "generate_datasets/tmp/digit-data/simple_add.npy"]
 data_mul = np.load(data_fp[0], allow_pickle=True)
@@ -37,14 +41,10 @@ data_len = len(data)
 train_split_idx = int(data_len * training_split)
 train_data = data[:train_split_idx]
 test_data = data[train_split_idx:]
-
-admu = HandleAddMul(input_dims, output_dims, dir=network_cache_dir+netwrok_name, checkpoint=checkpoint, lr=0.001)
-
 train_loader = T.utils.data.DataLoader(dataset=T.tensor(train_data),batch_size=batchsize,
                                           shuffle=True)
 test_loader = T.utils.data.DataLoader(dataset=T.Tensor(test_data),batch_size=batchsize,
                                           shuffle=True)
-
 def cycle(iterable):
     while True:
         for x in iterable:
@@ -53,11 +53,14 @@ def cycle(iterable):
 iterator_train = iter(cycle(train_loader))
 iterator_test = iter(cycle(test_loader))
 
-running_loss = 0.0
+
+'''Network Pretraining
+        HandleAddMull is the network (NOT mask) handler
+'''
+admu = HandleAddMul(input_dims, output_dims, dir=network_cache_dir+netwrok_name, checkpoint=checkpoint, lr=0.001)
+
 if not test_flag:
     for e in range(num_epochs):
-        # for idx, batch in enumerate(train_loader):
-        #     running_loss += admu.learn(batch)
         batch = next(iterator_train)
         loss = admu.learn(batch)
         print(f'Epoch {e} : Loss {loss}')
